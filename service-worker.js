4b53d4eeeedcbf16828a7651a86c118c52c80653
Oluwasanmi Israel-collins <148803978+Aminco6@users.noreply.github.com>
2025-01-16 20:26:21 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "esp-config-cache";
+const CACHE_NAME = "voddic-burglar-cache-v1";
 const urlsToCache = ["/", "/index.html", "/app.js", "/manifest.json"];
 
 self.addEventListener("install", (event) => {

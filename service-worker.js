b4781b91dc7aa9ecedffcf4010034689e357e168
Oluwasanmi Israel-collins <148803978+Aminco6@users.noreply.github.com>
2025-01-16 21:52:01 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_NAME = "voddic-burglar-cache-v1";
 const urlsToCache = [
-  "/Voddic-Burglar-Alarm-Basic/",  // Main URL path
+  "/Voddic-Burglar-Alarm-Basic/", // Main URL path
   "/Voddic-Burglar-Alarm-Basic/index.html",
   "/Voddic-Burglar-Alarm-Basic/app.js",
   "/Voddic-Burglar-Alarm-Basic/manifest.json",
-  "/Voddic-Burglar-Alarm-Basic/icon-192x192.png",
-  "/Voddic-Burglar-Alarm-Basic/icon-512x512.png"
+  "/Voddic-Burglar-Alarm-Basic/icon-placeholder.png"
 ];
 
+// Installing service worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -16,6 +16,7 @@ self.addEventListener("install", (event) => {
   );
 });
 
+// Fetching from cache
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {

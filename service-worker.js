c5751cd11edc3fcd0bb96e082bbff36f833a6a20
Oluwasanmi Israel-collins <148803978+Aminco6@users.noreply.github.com>
2025-01-16 21:26:35 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "voddic-burglar-cache-v1";
 const urlsToCache = [
-  "/Voddic-Burglar-Alarm-Basic/",  // Make sure this is the correct path
+  "/Voddic-Burglar-Alarm-Basic/",  // Main URL path
   "/Voddic-Burglar-Alarm-Basic/index.html",
   "/Voddic-Burglar-Alarm-Basic/app.js",
   "/Voddic-Burglar-Alarm-Basic/manifest.json",

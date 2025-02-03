2082da82f7e7fe4348bc725211fc542cacf5263d
Oluwasanmi Israel-collins <148803978+Aminco6@users.noreply.github.com>
2025-02-03 22:06:12 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,28 @@
-const cacheName = "voddic-burglar-config-v1";
+const cacheName = "voddic-burglar-config-v2";
 const assetsToCache = [
-  "/Voddic-Burglar-Alarm-Basic/", // Main URL path
-  "/Voddic-Burglar-Alarm-Basic/index.html",
-  "/Voddic-Burglar-Alarm-Basic/app.js",
-  "/Voddic-Burglar-Alarm-Basic/manifest.json",
-  "/Voddic-Burglar-Alarm-Basic/VODDICS.png"
+  "/", // Main URL path
+  "/index.html",
+  "/app.js",
+  "/manifest.json",
+  "/VODDICS.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
       return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== cacheName) // Remove old cache
+          .map((name) => caches.delete(name))
+      );
     })
   );
 });
